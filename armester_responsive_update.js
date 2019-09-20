ac03abdb8704a2991eb77ea81130be3bd8bcbeff
jimbcs <55375194+jimbcs@users.noreply.github.com>
2019-09-20 06:28:34 +0200

    Update armester_responsive_update.js

--- armester_responsive_update.js
+++ armester_responsive_update.js
@@ -1,5 +1,5 @@
 function ph_is_site_responsive(hostname)
 {
-    var sites = ["prohardver.hu"];
+    var sites = ["prohardver.hu, "mobilarena.hu", "gamepod.hu", "logout.hu", "itcafe.hu", "fototrend.hu"];
     return sites.indexOf(hostname) >= 0;
 }
