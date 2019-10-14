3e3a24ef78a03c0795160bb454ed133b659eb3ee
jimbcs <55375194+jimbcs@users.noreply.github.com>
2019-10-14 17:57:57 +0200

    Update armester_responsive_2019_2.0_jimbcs.js

--- armester_responsive_2019_2.0_jimbcs.js
+++ armester_responsive_2019_2.0_jimbcs.js
@@ -6,7 +6,7 @@
 // @include      /^http(s)?://(itcafe|prohardver|mobilarena|fototrend)\.hu/(tema|privat)/
 // @updateURL    https://raw.githubusercontent.com/jimbcs/Prohardver/master/armester_responsive_2019_2.0_jimbcs.js
 // @grant        none
-// @require      https://raw.githubusercontent.com/jimbcs/Prohardver/master/armester_responsive_update.js
+// @require      https://raw.githubusercontent.com/jimbcs/Prohardver/master/armester_responsive_2019_2.0_jimbcs_update.js
 
 // ==/UserScript==
 (function() {
